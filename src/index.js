6f68f4e3eb6b4a1605764d906c22abcf29e844a7
rosen777 <rosen.toshev@flatironschool.com>
2018-12-17 14:34:04 +0000

    Done.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+const taskForm = document.querySelector('#create-task-form')
+const taskDescriptionEl = document.querySelector('input[name=new-task-description]')
+const tasksList = document.querySelector('#tasks')
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(e)
+  const taskListEl = document.createElement('li')
+  taskListValue = taskDescriptionEl.value
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = 'X'
+  deleteButton.addEventListener('click', () => {
+    console.log(taskListEl)
+    taskListEl.remove()
+  })
+  taskListEl.append(taskListValue, deleteButton)
+
+  tasksList.append(taskListEl)
+
+
+  // const buttonDelete = document.querySelector('#btn-delete')
+  // buttonDelete.addEventListener('click', () => {
+  //       console.log(taskListEl)
+  //       taskListEl.remove()
+  //     })
+
+  })
+
+})
